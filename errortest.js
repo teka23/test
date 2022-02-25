ee8ba48fc560d124467222c9eaaae6e9a2ea3de7
Temuulen Enkhee <temulen.mr@gmail.com>
2022-02-25 18:44:46 +0900

    Update errortest.js

--- errortest.js
+++ errortest.js
@@ -3,6 +3,7 @@ looker.plugins.visualizations.add({
     
   },
   updateAsync: function(data, element, config, queryResponse, details, done) {
+      console.log("test")
       this.addError({title: "No Dimensions", message: "This chart requires dimensions."});
       return;
   }
